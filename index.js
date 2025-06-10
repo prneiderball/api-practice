0bf2c4b7e36cffb93d1d153b9d3a15d62c69104a
P.R. Neider-Ball <phillrballn@icloud.com>
2025-06-10 17:43:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const app = require('express')();
-const PORT = 8080
+const express = require('express');
+const app = express();
+const PORT = 8080;
 
-app.listen (
-    PORT,
-    console.log(`It's aliv on http://localhost:${PORT}`)
-);
+app.use(express.json());
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'Large'
+    });
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        return res.status(418).send({ message: 'We need a logo!' });
+    }
+
+    res.send({
+        tshirt: `👕 with your ${logo} and ID of ${id}`,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`It's alive on http://localhost:${PORT}`);
+});
